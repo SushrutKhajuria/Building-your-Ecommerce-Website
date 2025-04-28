26e92cdd934f2c029435259b73f571eebaa5c11b
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-04-28 23:00:15 +0530

    Updating the Old Password

--- Authentication-main/Authentication-main/src/components/Profile/ProfileForm.js
+++ Authentication-main/Authentication-main/src/components/Profile/ProfileForm.js
@@ -1,17 +1,66 @@
+import { useRef, useState, useContext } from 'react';
+import { AuthContext } from '../../store/auth-context';
+import { useHistory } from 'react-router-dom';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    try {
+      const response = await fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAclndL_CUBso12JBTmsGUFUOUpe1mWJ38',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            idToken: authCtx.token, 
+            password: enteredNewPassword,
+            returnSecureToken: true,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.error.message || 'Password change failed!');
+      }
+
+      history.replace('/');
+    } catch (err) {
+      alert(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
       </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
+      <div className={classes.actions}>
+        <button disabled={isLoading}>
+          {isLoading ? 'Changing Password...' : 'Change Password'}
+        </button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
