04767e5d9a6329fe18d963d001b00d902b243d52
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-04-28 23:20:20 +0530

    Implementing Logout functionality

--- Authentication-main/Authentication-main/src/components/Layout/MainNavigation.js
+++ Authentication-main/Authentication-main/src/components/Layout/MainNavigation.js
@@ -1,34 +1,31 @@
-import { Link } from 'react-router-dom';
 import { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { AuthContext } from '../../store/auth-context';
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
+  const history = useHistory();
 
-  if (authCtx.token === undefined) return null; // Or a loading spinner
-
+  const logoutHandler = () => {
+    authCtx.logout();  
+    history.replace('/auth'); 
+  };
 
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
           {!authCtx.isLoggedIn && (
-            <li>
-              <Link to='/auth'>Login</Link>
-            </li>
+            <li><Link to="/auth">Login</Link></li>
           )}
           {authCtx.isLoggedIn && (
             <>
-              <li>
-                <Link to='/profile'>Profile</Link>
-              </li>
-              <li>
-                <button onClick={authCtx.logout}>Logout</button>
-              </li>
+              <li><Link to="/profile">Profile</Link></li>
+              <li><button onClick={logoutHandler}>Logout</button></li>
             </>
           )}
         </ul>
